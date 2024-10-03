8dc659d6bee68bb437e97fe438f52300e751517f
Akuseru07 <axelgamer0507@gmail.com>
2024-10-03 14:37:52 -0700

    Configuracion del Proyecto

--- SistemaInventarioV6/Program.cs
+++ SistemaInventarioV6/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using SistemaInventarioV6.Data;
+using SistemaInventarioV6.AccesoDatos.Data;
 
 namespace SistemaInventarioV6
 {
@@ -18,7 +18,7 @@ namespace SistemaInventarioV6
 
             builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
-            builder.Services.AddControllersWithViews();
+            builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
             var app = builder.Build();
 
@@ -43,7 +43,7 @@ namespace SistemaInventarioV6
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{area=Inventario}/{controller=Home}/{action=Index}/{id?}");
             app.MapRazorPages();
 
             app.Run();
