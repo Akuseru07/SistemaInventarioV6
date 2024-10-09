b9e6b9185155802006a2646c0a6c785f56c851e8
Akuseru07 <axelgamer0507@gmail.com>
2024-10-09 09:39:01 -0700

    CRUD de Marca

---
+++ SistemaInventario.AccesoDatos/Configuracion/MarcaConfiguracion.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SistemaInventario.Modelos;
+
+namespace SistemaInventarioV6.Configuracion
+{
+    //FLUENT API
+    public class MarcaConfiguracion : IEntityTypeConfiguration<Marca>
+    {
+        public void Configure(EntityTypeBuilder<Marca> builder)
+        {
+            builder.Property(x => x.Id).IsRequired();
+            //builder.Property(x => x.Nombre).IsRequired(false); si no es requerida se podria poner asi
+            builder.Property(x => x.Nombre).IsRequired().HasMaxLength(60);
+            builder.Property(x => x.Descripcion).IsRequired().HasMaxLength(100);
+            builder.Property(x => x.Estado).IsRequired();
+        }
+    }
+}
---
+++ SistemaInventario.AccesoDatos/Repositorio/MarcaRepositorio.cs
@@ -0,0 +1,32 @@
+﻿using SistemaInventario.AccesoDatos.Repositorio.IRepositorio;
+using SistemaInventario.Modelos;
+using SistemaInventarioV6.AccesoDatos.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.AccesoDatos.Repositorio
+{
+    public class MarcaRepositorio : Repositorio<Marca>, IMarcaRepositorio
+    {
+        private readonly ApplicationDbContext _db;
+
+        public MarcaRepositorio(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Actualizar(Marca marca)
+        {
+            var marcaBD = _db.Marcas.FirstOrDefault(b => b.Id == marca.Id); //Se captura el registro antes de actualizarlo
+            if(marcaBD != null)
+            {
+                marcaBD.Nombre= marca.Nombre;
+                marcaBD.Descripcion = marca.Descripcion;
+                marcaBD.Estado = marca.Estado;
+                _db.SaveChanges(); //Creo que no ocupo ponerlo, pero sirve para salvar los cambios
+            }
+        }
+    }
+}
