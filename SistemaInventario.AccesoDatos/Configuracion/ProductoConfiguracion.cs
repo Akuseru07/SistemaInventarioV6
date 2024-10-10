5abef18a7476731fa7225633b36c8963f53316a6
Akuseru07 <axelgamer0507@gmail.com>
2024-10-10 11:52:59 -0700

    CRUD de Producto

---
+++ SistemaInventario.AccesoDatos/Configuracion/ProductoConfiguracion.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SistemaInventario.Modelos;
+
+namespace SistemaInventarioV6.Configuracion
+{
+    //FLUENT API
+    public class ProductoConfiguracion : IEntityTypeConfiguration<Producto>
+    {
+        public void Configure(EntityTypeBuilder<Producto> builder)
+        {
+            builder.Property(x => x.Id).IsRequired();
+            //builder.Property(x => x.Nombre).IsRequired(false); si no es requerida se podria poner asi
+            builder.Property(x => x.NumeroSerie).IsRequired().HasMaxLength(60);
+            builder.Property(x => x.Descripcion).IsRequired().HasMaxLength(100);
+            builder.Property(x => x.Estado).IsRequired();
+            builder.Property(x => x.Precio).IsRequired();
+            builder.Property(x => x.Costo).IsRequired();
+            builder.Property(x => x.CategoriaId).IsRequired();
+            builder.Property(x => x.MarcaId).IsRequired();
+            builder.Property(x => x.ImagenUrl).IsRequired(false);
+            builder.Property(x => x.PadreId).IsRequired(false);
+
+            //Relaciones
+            //HasOne ed de uno y withmany a muchos
+            builder.HasOne(x => x.Categoria).WithMany()
+                .HasForeignKey(x => x.CategoriaId)
+                .OnDelete(DeleteBehavior.NoAction);
+            builder.HasOne(x => x.Marca).WithMany()
+                .HasForeignKey(x => x.MarcaId)
+                .OnDelete(DeleteBehavior.NoAction);
+            
+            builder.HasOne(x => x.Padre).WithMany()
+                .HasForeignKey(x => x.PadreId)
+                .OnDelete(DeleteBehavior.NoAction);
+            //Si yo no quiero que me afecte el caso de borrado en cascada le agregamos el ondelete o algo asi
+
+        }
+    }
+}
---
+++ SistemaInventario.Modelos/Producto.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.Modelos
+{
+    public class Producto
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Numero de Serie es Requerido")]
+        [MaxLength(60)]
+        public string NumeroSerie { get; set; }
+        [Required(ErrorMessage = "Descripcion es Requerida")]
+        [MaxLength(60)]
+        public string Descripcion { get; set; }
+        [Required(ErrorMessage ="Precio es requerido")]
+        public double Precio { get; set; }
+        [Required(ErrorMessage = "Costo es requerido")]
+        public double Costo { get; set; }
+
+        //La imagen no es requerida ya que se guarda cuando estamos grabando el producto
+        public string ImagenUrl { get; set; }
+        [Required(ErrorMessage ="Estado es requerido")]
+        public bool Estado { get; set; }
+
+        [Required(ErrorMessage ="Categoria es Requerido")]
+        //Para poder hacer la foreign key, se hace esto para poder obtener las propiedades de categoria
+        public int CategoriaId { get; set; }
+        [ForeignKey("CategoriaId")]
+        public Categoria Categoria { get; set; }
+
+        [Required(ErrorMessage ="Marca es Requerido")]
+        public int MarcaId { get; set; }
+        [ForeignKey("MarcaId")]
+        public Marca Marca { get; set; }
+
+        //Relacion de recursividad, coy a tener una propiedad padre, la cual se va a relacionar con un mismo ID
+        //de mi modelo
+        //El int? es para que se grabe como nulo obligatoriamente
+
+        public int? PadreId { get; set; }
+        //Navegacion
+        public virtual Producto Padre { get; set; } //un producto puede estar relacionado a un mismo producto y ese va a ser el padre
+
+    }
+}
